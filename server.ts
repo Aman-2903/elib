cf6de910bf834fb8f890c5ee1e256ebdfb7496c2
Aman-2903 <kushwahaaman075@gmail.com>
2024-06-01 13:33:03 +0530

    add database setup

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import { config } from "./src/config/config";
 import app from "./src/app";
+import connectDB from "./src/config/db";
 
 console.log(config.port);
 
-const startServer = () => {
+const startServer = async () => {
+  //connect database
+  await connectDB();
+
   const port = config.port || 4000;
 
   app.listen(port, () => {
